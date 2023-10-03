3e4a05a2502385aaa2a902387bd54b88cf39ecbd
Louis-Marie Michelin <lmichelin@outlook.fr>
2023-10-03 20:41:22 +0200

    add hostname to env variables

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bodyParser: false })
 
   const port = process.env.PORT ?? 3000
-  await app.listen(port)
+  const hostname = process.env.HOSTNAME ?? "0.0.0.0"
+  await app.listen(port, hostname)
   console.log(`App listening on port ${port}`)
 }
 
